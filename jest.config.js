41522d2d3b37b84e3fe534ecf5269b6f722a4fe8
Vinicius Boscoa <virb30@gmail.com>
2020-07-29 22:32:38 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  roots: ["<rootDir>/src"],
-  coverageReporters: ["text-summary", "lcov"],
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  roots: ['<rootDir>/src'],
+  coverageReporters: ['text-summary', 'lcov'],
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest",
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
