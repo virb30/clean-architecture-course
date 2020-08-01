af820331380c725ffdb2fa1436daccc1438d0d5c
Vinicius Boscoa <virb30@gmail.com>
2020-08-01 10:59:10 -0300

    chore: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   coverageDirectory: 'coverage',
   roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '!<rootDir>/src/**/*protocols.ts',
+    '!<rootDir>/src/**/protocols/**',
+    '!<rootDir>/src/main/**'
+  ],
   coverageReporters: ['text-summary', 'lcov'],
   testEnvironment: 'node',
   transform: {
