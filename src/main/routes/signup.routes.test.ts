ff7f661f94affac4824381c97ffd61a0dc8e12b0
Vinicius Boscoa <virb30@gmail.com>
2020-08-01 10:17:17 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup.routes.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        password: '123456',
+        passwordConfirmation: '123456'
+      })
+      .expect(200)
+  })
+})
